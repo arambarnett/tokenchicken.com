8252256bf4aa5dbd5d8b99807ebefa1efc604d35
Aram Barnett <aram.barnett@gmail.com>
2018-04-04 15:33:44 -0400

    Delete ERC721Holder.sol

--- ERC721Holder.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.18;
-
-import './IERC721Receiver.sol';
-
-contract ERC721Holder is IERC721Receiver {
-  function onERC721Received(uint256 /* tokenId */, address /* oldOwner */, bytes /* data */) public {
-    return;
-  }
-}
